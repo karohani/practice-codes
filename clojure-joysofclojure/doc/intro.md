5ec6bcb218e846e00661cf97214688508985306d
karohani <wilzilzor@gmail.com>
2022-11-13 01:55:20 +0900

    clojure

---
+++ clojure-joysofclojure/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to joysofclojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure-joysofclojure/src/joysofclojure/core.clj
@@ -0,0 +1,10 @@
+(ns joysofclojure.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(def a "1234")
+
+(String. "1234")
