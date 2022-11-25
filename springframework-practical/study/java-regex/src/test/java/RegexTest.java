6ac66e069b2af7e841d8942ef5b1033e379fc9f4
jae.gawata <jae.gawata@kakaopaycorp.com>
2022-11-25 16:04:46 +0900

    How to Java Regex

--- springframework-practical/study/java-regex/src/test/java/RegexTest.java
+++ springframework-practical/study/java-regex/src/test/java/RegexTest.java
@@ -3,6 +3,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class RegexTest {
@@ -45,10 +46,49 @@ public class RegexTest {
 
     @Test
     public void 개행_뒤에_여러_문자테스트() {
-        String pattern = "ABCD\\n";
-        String target = "ABCD\\nABCD\\n";
+        String pattern = "ABCD\n";
+        String target = "ABCD\nABCD\\n";
         Pattern compile = Pattern.compile( pattern );
         String[] split = compile.split( target );
-        Arrays.stream( split ).forEach( System.out::println );
+        System.out.println( split.length );
+    }
+
+    @Nested
+    class GroupTest {
+
+        @Test
+        public void test01() {
+            String line = "This order was placed for QT3000! OK?";
+            String pattern = "(.*)(\\d+)(.*)";
+            // Create a Pattern object
+            Pattern r = Pattern.compile( pattern );
+
+            // Now create matcher object.
+            Matcher m = r.matcher( line );
+
+            if (m.find()) {
+                System.out.println( "Found value: " + m.group( 0 ) );
+                System.out.println( "Found value: " + m.group( 1 ) );
+                System.out.println( "Found value: " + m.group( 2 ) );
+            } else {
+                System.out.println( "NO MATCH" );
+            }
+        }
+
+        @Test
+        public void test02() {
+            String line = "대한민국 월드컵은 2002년에 있었다.";
+            Pattern numReg = Pattern.compile( "\\d" );
+            Matcher matcher = numReg.matcher( line );
+
+            assertThat( matcher.find() ).isTrue();
+            assertThat( matcher.group() ).isEqualTo( "2" );
+            assertThat( matcher.find() ).isTrue();
+            assertThat( matcher.group() ).isEqualTo( "0" );
+            assertThat( matcher.find() ).isTrue();
+            assertThat( matcher.group() ).isEqualTo( "0" );
+            assertThat( matcher.find() ).isTrue();
+            assertThat( matcher.group() ).isEqualTo( "2" );
+        }
     }
 }
